0b07d07b92a6d270a00152402491fb7b8d0dcc6f
Tayyab Hussain <tayyab.hussain@olimpwarehousing.com>
2023-12-30 22:54:25 +0500

    Changes to be committed: 	modified:   index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <title>Movie Search</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/custom.css" rel="stylesheet">
+    <link href="css/custom.css" rel="stylesheet">
 </head>
 
 <body>
